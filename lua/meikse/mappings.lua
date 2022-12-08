8e1ccfe2138ed10aba1915a2434e34000024e6bb
Meik Seedorf <meik_seedorf@web.de>
2022-12-08 16:30:05 +0100

    buffer overview

--- lua/meikse/mappings.lua
+++ lua/meikse/mappings.lua
@@ -1,3 +1,5 @@
 -- go to next/previous buffers
 vim.keymap.set('n','<C-p>','<cmd>bp<cr>', { silent = true })
 vim.keymap.set('n','<C-n>','<cmd>bn<cr>', { silent = true })
+
+vim.keymap.set('n','<leader><leader>','<cmd>ls<cr>')
