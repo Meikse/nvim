3c82df4602047bd638de71e57d27a2ebbe19b929
Meik Seedorf <meik_seedorf@web.de>
2022-12-08 16:30:30 +0100

    added git and translator

---
+++ after/plugin/pantran.lua
@@ -0,0 +1,4 @@
+-- local opts = {noremap = true, silent = true, expr = true}
+-- vim.keymap.set("n", "<leader>tr", pantran.motion_translate, opts)
+-- vim.keymap.set("n", "<leader>trr", function() return pantran.motion_translate() .. "_" end, opts)
+-- vim.keymap.set("x", "<leader>tr", pantran.motion_translate, opts)
---
+++ after/plugin/vim-fugitive.lua
@@ -0,0 +1,16 @@
+vim.keymap.set('n','<leader>gs','<cmd>Git status<cr>')
+vim.keymap.set('n','<leader>gt','<cmd>Git tree<cr>')
+vim.keymap.set('n','<leader>ga','<cmd>Git add<cr>')
+vim.keymap.set('n','<leader>gc','<cmd>Git commit<cr>')
+vim.keymap.set('n','<leader>go','<cmd>Git checkout<cr>')
+vim.keymap.set('n','<leader>gm','<cmd>Git merge<cr>')
+vim.keymap.set('n','<leader>gb','<cmd>Git branch<cr>')
+
+
+-- noremap <silent> <leader>gs :Git status<cr>
+-- noremap <silent> <leader>gt :Git tree<cr>
+-- noremap <silent> <leader>ga :Git add %<cr>
+-- noremap <silent> <leader>gc :Git commit %<cr>
+-- noremap <leader>go :Git checkout 
+-- noremap <leader>gm :Git merge 
+-- noremap <leader>gb :Git branch 
