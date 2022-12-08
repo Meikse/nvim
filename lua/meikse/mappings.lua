fe2b4df0636d9c2596eee458de019ab465509ecc
Meik Seedorf <meik_seedorf@web.de>
2022-12-08 16:40:51 +0100

    tab switching

--- lua/meikse/mappings.lua
+++ lua/meikse/mappings.lua
@@ -2,4 +2,9 @@
 vim.keymap.set('n','<C-p>','<cmd>bp<cr>', { silent = true })
 vim.keymap.set('n','<C-n>','<cmd>bn<cr>', { silent = true })
 
+-- buffer overview
 vim.keymap.set('n','<leader><leader>','<cmd>ls<cr>')
+
+-- buffer switching
+vim.keymap.set('n','<Tab>',':b<space>')
+
