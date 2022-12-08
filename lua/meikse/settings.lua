cf9b04fdabf632e7325063e07aef6ed3d964a5b1
Meik Seedorf <meik_seedorf@web.de>
2022-12-08 18:18:57 +0100

    added basic settings

--- lua/meikse/settings.lua
+++ lua/meikse/settings.lua
@@ -1,5 +1,21 @@
+-- map to system clipboard
 vim.opt.clipboard = "unnamedplus"
 
+-- mapleader
 vim.g.mapleader = ' '
 
+-- linenumbering
+vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- working dir
+vim.opt.autochdir = true
+
+-- tabbing
+vim.opt.tabstop = 4
+vim.opt.softtabstop= 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- indenting
+vim.opt.smartindent = true
