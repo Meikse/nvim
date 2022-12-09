4bffa389416716a2b70ba52c0c0975aca54a5433
Meik Seedorf <meik_seedorf@web.de>
2022-12-09 01:44:01 +0100

    vimtex minimal config

---
+++ after/plugin/vimtex.lua
@@ -0,0 +1,4 @@
+vim.g.vimtex_quickfix_open_on_warning = false
+vim.keymap.set('n','<leader>lc','<cmd>VimtexCompile<cr>')
+vim.keymap.set('n','<leader>lt','<cmd>VimtexTocToggle<cr>')
+vim.keymap.set('n','<leader>ls','<cmd>VimtexStatus<cr>')
