5c259e156cc471fb5b2afbbd39123876e916237a
Meik Seedorf <meik_seedorf@web.de>
2022-12-10 15:39:32 +0100

    cleaned up

--- after/plugin/vim-fugitive.lua
+++ after/plugin/vim-fugitive.lua
@@ -5,12 +5,3 @@ vim.keymap.set('n','<leader>gc','<cmd>Git commit %<cr>')
 vim.keymap.set('n','<leader>go','<cmd>Git checkout<cr>')
 vim.keymap.set('n','<leader>gm','<cmd>Git merge<cr>')
 vim.keymap.set('n','<leader>gb','<cmd>Git branch<cr>')
-
-
--- noremap <silent> <leader>gs :Git status<cr>
--- noremap <silent> <leader>gt :Git tree<cr>
--- noremap <silent> <leader>ga :Git add %<cr>
--- noremap <silent> <leader>gc :Git commit %<cr>
--- noremap <leader>go :Git checkout 
--- noremap <leader>gm :Git merge 
--- noremap <leader>gb :Git branch 
