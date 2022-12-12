793a693afa5fb36ea6718520aa5e32902eb2f907
Meik Seedorf <meik_seedorf@web.de>
2022-12-12 01:13:15 +0100

    changed mapping

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,2 +1,2 @@
 -- for learning basics
-vim.api.nvim_buf_set_keymap(0,'n', '<F5>', ':w %<CR>:!g++ %<cr>:!./a.out<cr>', { noremap = true })
+vim.api.nvim_buf_set_keymap(0,'n', '<F4>', ':w %<CR>:!g++ %<cr>:!./a.out<cr>', { noremap = true })
--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,1 +1,1 @@
-vim.api.nvim_buf_set_keymap(0,'n', '<F5>', ':w %<cr>:!python3 %<CR>', { noremap = true })
+vim.api.nvim_buf_set_keymap(0,'n', '<F4>', ':w %<cr>:!python3 %<CR>', { noremap = true })
