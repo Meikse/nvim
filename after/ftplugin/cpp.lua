f8eb4bfbe7fb50fa69fca01dde9a1d39f83e4dd2
Meik Seedorf <meik_seedorf@web.de>
2022-12-13 17:21:40 +0100

    80 columns line marker

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -2,3 +2,5 @@
 vim.api.nvim_buf_set_keymap(0,'n', '<F4>', ":w %<CR>:exec '!g++' shellescape(@%,1) '-o' shellescape(expand('%:r'),1)<CR>:exec '!./'.shellescape(expand('%:r'))<CR>", { noremap = true })
 -- vim.api.nvim_buf_set_keymap(0,'n', '<F4>', ':w %<CR>:!g++ %<cr>:!./a.out<cr>', { noremap = true })
 -- @ == expand()
+
+vim.opt.colorcolumn="80"
--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,1 +1,3 @@
 vim.api.nvim_buf_set_keymap(0,'n', '<F4>', ':w %<cr>:!python3 %<CR>', { noremap = true })
+
+vim.opt.colorcolumn="80"
