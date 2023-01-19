7dfd887ac43fec8cf106e80df58a4ab6e79f2ce6
Meik Seedorf <meik_seedorf@web.de>
2023-01-19 15:25:41 +0100

    extended keymap functionality

--- after/plugin/vim-translator.lua
+++ after/plugin/vim-translator.lua
@@ -1,8 +1,11 @@
 vim.g.translator_target_lang = 'de'
 vim.g.translator_default_engines = {'google'}
--- translate one word and insert
+-- translate one word and info
 vim.keymap.set('n','<leader>td',':Translate --target_lang=de<cr>')
 vim.keymap.set('n','<leader>te',':Translate --target_lang=en<cr>')
--- translate one word and info
+-- translate one word and insert
 vim.keymap.set('n','<leader>tw','viw:TranslateR --target_lang=en<cr>')
 vim.keymap.set('n','<leader>ts','viw:TranslateR --target_lang=de<cr>')
+-- translate sentence/words and insert
+vim.keymap.set('v','<leader>tw',':TranslateR --target_lang=en<cr>')
+vim.keymap.set('v','<leader>ts',':TranslateR --target_lang=de<cr>')
