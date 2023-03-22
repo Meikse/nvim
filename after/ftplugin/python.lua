918222be5cc321742373e78ce5a465a1a6a12b13
Meik Seedorf <meik_seedorf@web.de>
2023-03-22 22:45:22 +0100

    improved F5 keymap for jupyter

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,5 +1,7 @@
 vim.api.nvim_buf_set_keymap(0,'n', '<F4>', ':w %<cr>:!python3 %<CR>', { noremap = true })
-vim.api.nvim_buf_set_keymap(0,'n', '<F5>', ':w %<cr>:JupyterRunFile <CR>', { noremap = true })
+-- map <F5> :w <cr> :JupyterSendCell <cr>
+vim.api.nvim_buf_set_keymap(0,'n', '<F5>', ':w %<cr>:JupyterSendCell <CR>', { noremap = true })
 -- autocmd Filetype python map <buffer> <F5> :w<cr>:JupyterRunFile <cr>:exec '!python3' shellescape(@%, 1)<cr>
+--
 
 vim.opt.colorcolumn="80"
