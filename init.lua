2a28fd32b64db4af2b4be5817c8360f0fe964d64
Meik Seedorf <meik_seedorf@web.de>
2023-07-02 10:53:06 +0200

    hard coded node version for Copilot

--- init.lua
+++ init.lua
@@ -1,1 +1,11 @@
 require("meikse")
+
+-- Copilot needs newer version of node (dirty)
+vim.env.NEOVIM_NODE_VERSION = 'v16.13.1'
+
+if vim.fn.has('unix') and vim.env.NEOVIM_NODE_VERSION then
+    local node_dir = vim.env.HOME .. '/.nvm/versions/node/' .. vim.env.NEOVIM_NODE_VERSION .. '/bin/'
+    if (vim.fn.isdirectory(node_dir)) then
+        vim.env.PATH = node_dir .. ':' .. vim.env.PATH
+    end
+end
